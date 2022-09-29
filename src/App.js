43d4ed5e3fd2e015be4da31322581f9288614d6e
Reenadivya <reenad286@gmail.com>
2022-09-30 03:31:28 +0800

    added header, links, and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">testing</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/search" element={<Search />} />
+          {/* Search Page */}
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
